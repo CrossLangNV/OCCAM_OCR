8ba62d261cbb96faa26468340d288cd7e46249de
laurens <laurensmeeus93@gmail.com>
2021-08-17 17:10:58 +0200

    docker with GPU support

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OCCAM_OCR
+
+Documentation at http://localhost:9060/swagger.
--- backend/dbuild.sh
+++
@@ -1,7 +0,0 @@
-nvidia-docker build \
---no-cache \
--t django_pero_ocr .
-
-#docker build \
-#--no-cache \
-#-t django_test .
